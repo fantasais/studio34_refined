50bfc5dcc9a7e1553cd29d17c5d9748e75b9ab1f
fantasais <fantas.ai.sart@gmail.com>
2026-04-08 11:11:45 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  logLevel: "error",
+  build: {
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: "esbuild",
+    target: "es2019",
+  },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
